db2283c105f2f2df22e01a79f87e74ecdaf3660f
a663469 <a663469@gmail.com>
2025-05-18 21:07:30 +0300

    Laravel создание Блога 2.

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,18 @@
 <?php
 
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
+Route::group(['namespace' => '\App\Http\Controllers\Main'], function () {
+    Route::get('/', 'IndexController')->name('main.index');
+//    Route::get('/posts/create', 'CreateController')->name('post.create');
+//    Route::post('/posts', 'StoreController')->name('post.store');
+//    Route::get('/posts/{post}', 'ShowController')->name('post.show');
+//    Route::get('/posts/{post}/edit', 'EditController')->name('post.edit');
+//    Route::patch('/posts/{post}', 'UpdateController')->name('post.update');
+//    Route::delete('/posts/{post}', 'DestroyController')->name('post.delete');
 });
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
